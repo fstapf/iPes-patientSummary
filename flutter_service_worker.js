87e6f5fdc6a8215fd5d5bd38743bd0559d85c151
fstapf <fs.amancio@yahoo.com.br>
2025-10-31 10:40:17 -0300

    Deploy latest Flutter web build to gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "103cadf65995b3788268d73d349eb700
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "df37b1185ca927bab65d54dcac25d027",
+"flutter_bootstrap.js": "ffcad52874ef3b3a3f91dfe69bb0fbd3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
